6b64fbbc332ccc559cb9375ce8657864d2fcb536
david20571015 <david20571015@gmail.com>
2022-01-14 12:01:24 +0800

    first commit

---
+++ SP.v
@@ -0,0 +1,46 @@
+module SP(
+	// INPUT SIGNAL
+	clk,
+	rst_n,
+	in_valid,
+	inst,
+	mem_dout,
+	// OUTPUT SIGNAL
+	out_valid,
+	inst_addr,
+	mem_wen,
+	mem_addr,
+	mem_din
+);
+
+
+
+//------------------------------------------------------------------------
+//   INPUT AND OUTPUT DECLARATION                         
+//------------------------------------------------------------------------
+
+input                    clk, rst_n, in_valid;
+input             [31:0] inst;
+input  signed     [31:0] mem_dout;
+output reg               out_valid;
+output reg        [31:0] inst_addr;
+output reg               mem_wen;
+output reg        [11:0] mem_addr;
+output reg signed [31:0] mem_din;
+
+//------------------------------------------------------------------------
+//   DECLARATION
+//------------------------------------------------------------------------
+
+// REGISTER FILE, DO NOT EDIT THE NAME.
+reg	        [31:0] r      [0:31]; 
+
+
+
+//------------------------------------------------------------------------
+//   DESIGN
+//------------------------------------------------------------------------
+
+
+
+endmodule
